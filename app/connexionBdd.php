1d9772dae7deda04adaa724e58209c8bfab63485
Darlia51 <101891978+Darlia51@users.noreply.github.com>
2023-04-12 16:02:12 +0200

    Update connexionBdd.php

--- app/connexionBdd.php
+++ app/connexionBdd.php
@@ -1,8 +1,9 @@
 <?php
-	$dbhost = parse_url(getenv('STACKHERO_MYSQL_HOST'))['host'];
-	$dbuser = getenv('STACKHERO_MYSQL_USER');
-	$dbpass = getenv('STACKHERO_MYSQL_PASSWORD');
-	$dbname = getenv('STACKHERO_MYSQL_DATABASE');
+	$dbhost = $_ENV['STACKHERO_MYSQL_HOST'];
+	$dbuser = $_ENV['STACKHERO_MYSQL_USER'];
+	$dbpass = $_ENV['STACKHERO_MYSQL_PASSWORD'];
+	$dbname = $_ENV['STACKHERO_MYSQL_DATABASE'];
+
 	
 	$mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die(mysqli_error());
 ?>
