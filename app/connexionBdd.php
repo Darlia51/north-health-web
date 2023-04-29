faa5ed63f26d32ace87d4c6b34ad18aca2398dbd
Darlia51 <101891978+Darlia51@users.noreply.github.com>
2023-04-29 20:12:10 +0200

    Update connexionBdd.php

--- app/connexionBdd.php
+++ app/connexionBdd.php
@@ -5,25 +5,17 @@ $user = parse_url(getenv('STACKHERO_MYSQL_USER'));
 $password = parse_url(getenv('STACKHERO_MYSQL_ROOT_PASSWORD'));
 $database = parse_url(getenv('STACKHERO_MYSQL_DATABASE'));
 
-// Configuration SSL/TLS
-$sslOptions = array(
-    "ssl" => array(
-        "verify_peer" => false,
-        "verify_peer_name" => false,
-        "allow_self_signed" => true
-    )
-);
-
-// Connexion à la base de données avec SSL/TLS
 $conn = mysqli_init();
 $mysqliConnected = $mysqli->real_connect($hostname, $user, $password, $database, NULL, NULL, MYSQLI_CLIENT_SSL);
+if (!$mysqliConnected) {
+  die("Connect Error: " . $mysqli->connect_error());
 
 // Vérification de la connexion
 if (!$conn) {
     die("Connexion échouée: " . mysqli_connect_error());
 }
 
-echo "Connexion réussie";
+echo "Connexion réussie" . $conn->host_info . "\n";
 
 // Fermeture de la connexion
 $conn->close();
