38e0256e9a3d994fd1a9dd1baea1d18bf6ae9c55
Darlia51 <101891978+Darlia51@users.noreply.github.com>
2023-06-10 15:51:21 +0200

    Update dependance-ajax.php

--- config/dependance-ajax.php
+++ config/dependance-ajax.php
@@ -2,10 +2,10 @@
 // Include the database connection file
 include("./connexionBdd.php");
 
-if (isset($_POST['cityId']) && !empty($_POST['cityId'])) {
+if (isset($_POST['city']) && !empty($_POST['city'])) {
 
 	// Fetch establishment name base on city id -----------------------------
-	$query = "SELECT * FROM establishments WHERE idCity = ".$_POST['cityId'];
+	$query = "SELECT * FROM establishments WHERE idCity = ".$_POST['city'];
 	$result = $conn->query($query);
 
 	if ($result->num_rows > 0) {
@@ -16,10 +16,10 @@ if (isset($_POST['cityId']) && !empty($_POST['cityId'])) {
 	} else {
 		echo '<option value="">Establishment non disponible</option>';
 	}
-} elseif(isset($_POST['establishmentId']) && !empty($_POST['establishmentId'])) {
+} elseif(isset($_POST['establishment']) && !empty($_POST['establishment'])) {
 
 	// Fetch professionnal name base on establishment id ---------------------
-	$query = "SELECT * FROM professionnals WHERE idEstablishment = ".$_POST['establishmentId'];
+	$query = "SELECT * FROM professionnals WHERE idEstablishment = ".$_POST['establishment'];
 	$result = $conn->query($query);
 
 	if ($result->num_rows > 0) {
